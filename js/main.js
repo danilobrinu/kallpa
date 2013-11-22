c69f9dc905e157582a607099c1e907c54387bce7
danilobrinu <ddbn.c2@gmail.com>
2013-11-21 19:29:27 -0500

    subiendo nueva versionion mejorada de kallpa

--- js/main.js
+++ js/main.js
@@ -14,17 +14,16 @@ function transition(e) {
       $(this).animate({
         left: left
       }, time + (additionalTimeSign * (index * time / 6 ) ), ease);
-      console.log( additionalTimeSign * (index * time / 6 ) );
     });
   };
   var effectTime = 500,
     ease = 'easeInExpo';
   var previousTransition = function() {
-    effect('.currentPage', effectTime + 120, -1, 960, 0, ease);
+    effect('.currentPage', effectTime + 120, -1, '100%', 0, ease);
     effect('.previousPage', effectTime + 120, -1, 0, effectTime / 2, ease);
   };
   var nextTransition = function(){
-    effect('.currentPage', effectTime, 1, -960, 0, ease);
+    effect('.currentPage', effectTime, 1, '-100%', 0, ease);
     effect('.nextPage', effectTime, 1, 0, effectTime / 2, ease);
   };
   var resetTransition = function() {
@@ -35,7 +34,7 @@ function transition(e) {
   };
   var transitionUpdate = function(delay) {
     setTimeout(function(){
-      $hiddenPage = $('.page[data-page=' + hiddenPageID + ']');
+      $hiddenPage = $('.page[data-index=' + hiddenPageID + ']');
       classChange($previousPage, 'previousPage', e.data.exchangePreviousPage);
       classChange($currentPage, 'currentPage', e.data.exchangeCurrentPage);
       classChange($nextPage, 'nextPage', e.data.exchangeNextPage);
@@ -44,63 +43,59 @@ function transition(e) {
     }, delay);
   };
   if (e.data.reverse == true) {
-    /**
-     * fix height body
-     */
-    $('.body').css({height: $(window).width() > 980 ? $previousPage.height() : 1600});
-    /**
-     * transition and update id of hidden page
-     */
     previousTransition();
-    hiddenPageID = $previousPage.data('page') > 1 ? ($previousPage.data('page') - 1) : pagesNumber;
+    hiddenPageID = $previousPage.data('index') > 1 ? ($previousPage.data('index') - 1) : pagesNumber;
   } else {
-    /**
-     * fix height body
-     */
-    $('.body').css({height: $(window).width() > 980 ? $nextPage.height() : 1600});
-    /**
-     * transition and update id of hidden page
-     */
     nextTransition();
-    hiddenPageID = $nextPage.data('page') < pagesNumber ? ($nextPage.data('page') + 1) : 1;
+    hiddenPageID = $nextPage.data('index') < pagesNumber ? ($nextPage.data('index') + 1) : 1;
   }
   transitionUpdate(effectTime * 2.5);
+  console.log($previousPage.data('index'));
 }
+/**
+ * load events and transitions
+ */
+$('#previousTransitionButton').on('click', {
+  reverse: true,
+  exchangePreviousPage: 'currentPage',
+  exchangeCurrentPage: 'nextPage',
+  exchangeNextPage: 'hiddenPage',
+  exchangeHiddenPage: 'previousPage'
+}, transition);
 
-//if ($(window).width() > 980) {
-  $('#previousTransitionButton').on('click', {
-    reverse: true,
-    exchangePreviousPage: 'currentPage',
-    exchangeCurrentPage: 'nextPage',
-    exchangeNextPage: 'hiddenPage',
-    exchangeHiddenPage: 'previousPage'
-  }, transition);
+$('#nextTransitionButton').on('click', {
+  exchangePreviousPage: 'hiddenPage',
+  exchangeCurrentPage: 'previousPage',
+  exchangeNextPage: 'currentPage',
+  exchangeHiddenPage: 'nextPage'
+}, transition);
 
-  $('#nextTransitionButton').on('click', {
-    exchangePreviousPage: 'hiddenPage',
-    exchangeCurrentPage: 'previousPage',
-    exchangeNextPage: 'currentPage',
-    exchangeHiddenPage: 'nextPage'
-  }, transition);
-//}
-function transitionMarquesine() {
-  var w = 0;
-  $('.inicio>nav>ul li').css('width', 170);
-  $('.inicio>nav>ul').css('width', ($('.inicio>nav>ul li').length * 170) < 960 ? 960 : $('.inicio>nav>ul li').length * 170);
-  setTimeout(function() {
-    $('.inicio>nav>ul').animate({
-      left: 960 - $(this).width()
-    }, 4 * 1000);
-  }, 1000);
-  setInterval(function() {
-    $('.inicio>nav>ul').animate({
-      left: 960 - $(this).width()
-    }, 4 * 1000).animate({
-      left: 0
-    }, 0);
-  }, 4 * 1000);
-}
-
-if ($(window).width() > 980) {
-  transitionMarquesine();
-}
+/**
+ * core animation marquesine
+ */
+var marquesineAnimation =  function() {
+  var $marquesine = $('.home>.marquesine>ul');
+  $marquesine.css({width: $marquesine.find('li').length * 130});
+  setTimeout(function(){
+    $marquesine
+      .animate({
+        left: $marquesine.parent().width() - $marquesine.width() - 138
+      }, 6 * 1000)
+      .animate({
+        left: 0
+      }, 250);
+    setInterval(function(){
+      if ($('.home').hasClass('currentPage')) {
+        $marquesine
+          .animate({
+            left: $marquesine.parent().width() - $marquesine.width() - 138
+          }, 6 * 1000)
+          .animate({
+            left: 0
+          }, 250);
+      }
+    }, 6.5 * 1000);
+    
+  }, 500);
+};
+$(window).load(marquesineAnimation);
